b683889188a8da51cb8b82c9134960a51c65900f
nashi-san <hamouda.ihsan@gmail.com>
2023-09-07 22:17:09 +0000

    python-import_modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    arguments = sys.argv[1:]
+    num_arguments = len(arguments)
+    if num_arguments == 0:
+        print("0 arguments.")
+    elif num_arguments == 1:
+        print("1 argument:")
+        print("1: {}".format(arguments[0]))
+    else:
+        print("{} arguments:".format(num_arguments))
+        for i, arg in enumerate(arguments, 1):
+            print("{}: {}".format(i, arg))
