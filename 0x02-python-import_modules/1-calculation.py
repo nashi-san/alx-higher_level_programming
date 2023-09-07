d1a09ba6a47625bb3ca832179ab07c2dfeefb615
nashi-san <hamouda.ihsan@gmail.com>
2023-09-07 22:07:06 +0000

    python-import_modules

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    from calculator_1 import add, sub, mul, div
+    add_result = add(a, b)
+    sub_result = sub(a, b)
+    mul_result = mul(a, b)
+    div_result = div(a, b)
+    print("{} + {} = {}".format(a, b, add_result))
+    print("{} - {} = {}".format(a, b, sub_result))
+    print("{} * {} = {}".format(a, b, mul_result))
+    print("{} / {} = {}".format(a, b, div_result))
