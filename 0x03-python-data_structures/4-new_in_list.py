2094cd04189f88bdab026bba3cc9dc65f271e86d
nashi-san <hamouda.ihsan@gmail.com>
2023-09-11 18:06:35 +0000

    python-data_structures

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    if my_list is None:
+        my_list = []
     n_list = my_list[:]
     if (idx > 0 and idx < len(my_list)):
         n_list[idx] = element
