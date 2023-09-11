8afa82aeebb156a479bc1f0ebc662200248b97aa
nashi-san <hamouda.ihsan@gmail.com>
2023-09-11 18:17:39 +0000

    python-data_structures

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
+    if my_list is None:
+        my_list = []
+    for i in range(len(matrix) - 1):
+        for j in range(len(matrix[i]) - 1):
             if (j != 0):
                 print(" ", end='')
             print("{}".format(matrix[i][j]), end='')
