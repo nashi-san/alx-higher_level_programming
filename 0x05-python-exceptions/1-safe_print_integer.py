8924923cdc6d15545e2c9b26c7745749e3c37445
nashi-san <hamouda.ihsan@gmail.com>
2023-09-25 22:54:07 +0000

    python-exceptions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,8 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return True
-    except ValueError:
+    except:
         return False
+    else:
+        return True
+    
