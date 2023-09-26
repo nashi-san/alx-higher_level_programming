95e9dc462bc095ced2f70a115429d5ec1a4e1740
nashi-san <hamouda.ihsan@gmail.com>
2023-09-26 23:26:31 +0000

    python-classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+""" This class represents a square"""
+
+
 class Square:
-    """
-    This class represents a square.
-    """
+    """ This class represents a square"""
     pass
