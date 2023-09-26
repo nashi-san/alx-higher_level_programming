a36afdedce8a4ca9bf1613bf6fe6571e22626451
nashi-san <hamouda.ihsan@gmail.com>
2023-09-26 23:27:55 +0000

    python-classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""This class represents a square"""
+
+
 class Square:
+    """This class represents a square"""
+
     def __init__(self, size):
+        """
+        Initializes a Square instance.
+
+        Args:
+            size: The size of the square.
+        """
         self.__size = size
