058c06c1b3fa0de5038ccac7d406fb0230d9e22a
nashi-san <hamouda.ihsan@gmail.com>
2023-09-27 00:35:51 +0000

    python-classes

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -28,6 +28,7 @@ class MagicClass:
         Raises:
             TypeError: If the radius is not a number (float or int).
         """
+        self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
