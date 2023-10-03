6af2a703d65d3b85795240721e2c96feff795251
nashi-san <hamouda.ihsan@gmail.com>
2023-10-03 22:58:11 +0000

    python-test_driven_development

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+"""
+multiply matrices module
+this module contains a function that multiply matrices
+
+"""
+
+
+def matrix_mul(m_a, m_b):
+    """
+    multiply m_a and m_b
+    """
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+
+    if len(m_a) == 0 or any(not isinstance(row, list) for row in m_a):
+        raise ValueError("m_a can't be empty or must be a list of lists")
+
+    if any(any(not isinstance(element, (int, float))) for element in row) for row in m_a:
+        raise TypeError("m_a should contain only integers or floats")
+
+    if len(set(len(row) for row in m_a)) > 1:
+        raise TypeError("each row of m_a must be of the same size")
+
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+
+    if len(m_b) == 0 or any(not isinstance(row, list) for row in m_b):
+        raise ValueError("m_b can't be empty or must be a list of lists")
+
+    if any(any(not isinstance(element, (int, float)) for element in row) for row in m_b):
+        raise TypeError("m_b should contain only integers or floats")
+
+    if len(set(len(row) for row in m_b)) > 1:
+        raise TypeError("each row of m_b must be of the same size")
+
+    result = []
+    for i in range(len(m_a)):
+        row = []
+        for j in range(len(m_b[0])):
+            element = sum(m_a[i][k] * m_b[k][j] for k in range(len(m_b)))
+            row.append(element)
+        result.append(row)
+
+    return result
