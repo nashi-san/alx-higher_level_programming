a6821cc09dc13c1ff87314455c1dd21be20567ee
nashi-san <hamouda.ihsan@gmail.com>
2023-10-03 18:21:35 +0000

    python-test_driven_development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,11 +2,15 @@
 
 """
 Add module documentation.
+it has one function add_integer
+
 """
 
 
 def add_integer(a, b=98):
-    """Adds two integers and returns the result."""
+    """
+    Adds two integers and returns the result.
+    """
 
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
