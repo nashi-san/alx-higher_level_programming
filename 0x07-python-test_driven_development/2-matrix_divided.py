df54fe5517902fbf0c5ab6d3fd877139b0ae8241
nashi-san <hamouda.ihsan@gmail.com>
2023-10-03 19:37:06 +0000

    python-test_driven_development

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,13 +6,17 @@ This module contains a function to divide all elements of a matrix.
 
 """
 
+
 def matrix_divided(matrix, div):
     """
     Divides all elements of a matrix by a divisor and returns a new matrix.
     """
 
-    if not all(isinstance(row, list) and all(isinstance(elem, (int, float)) for elem in row) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(row, list) and
+               all(isinstance(elem, (int, float)) for elem in row)
+               for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) "
+                        "of integers/floats")
 
     if len(set(len(row) for row in matrix)) > 1:
         raise TypeError("Each row of the matrix must have the same size")
---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Say My Name Module
+This module contains a function that prints the given first name and last name.
+"""
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints the given first name and last name.
+    """
+
+    if not isinstance(first_name, str) or not isinstance(last_name, str):
+        raise TypeError("first_name must be a string or last_name must be a string")
+
+    if last_name:
+        print("My name is {} {}".format(first_name, last_name))
+    else:
+        print("My name is {}".format(first_name))
