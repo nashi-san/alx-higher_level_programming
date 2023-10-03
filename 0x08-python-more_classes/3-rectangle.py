5ac23750d1e6134c6989d260cc89f8f1849a024e
nashi-san <hamouda.ihsan@gmail.com>
2023-10-03 00:00:52 +0000

    more classes

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -66,3 +66,15 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return 0
         return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """
+        Returns a string representation of the rectangle.
+        """
+        if self.width == 0 or self.height == 0:
+            return ""
+        else:
+            rectangle_str = ""
+            for _ in range(self.height):
+                rectangle_str += "#" * self.width + "\n"
+            return rectangle_str.rstrip()
