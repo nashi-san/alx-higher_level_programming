72619f496dc9afe61eac4df3f31239593b65e2ae
nashi-san <hamouda.ihsan@gmail.com>
2023-10-03 19:42:12 +0000

    python-everything_is_object

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -11,9 +11,11 @@ def say_my_name(first_name, last_name=""):
     Prints the given first name and last name.
     """
 
-    if not isinstance(first_name, str) or not isinstance(last_name, str):
-        raise TypeError("first_name must be a string or"
-                        "last_name must be a string")
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if last_name and not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
 
     if last_name:
         print("My name is {} {}".format(first_name, last_name))
