df9d504518d15c260fc68d8c50e60338502c3e5c
nashi-san <hamouda.ihsan@gmail.com>
2023-10-09 22:22:36 +0000

    python-inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,7 +4,5 @@
 
 class MyList(list):
     def print_sorted(self):
-        """
-        Prints the elements of the list in sorted order.
-        """
+        """ Prints the elements of the list in sorted order."""
         print(sorted(self))
