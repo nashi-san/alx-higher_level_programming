04c679a07ef94df8196c52da421e49b6f2a1def6
nashi-san <hamouda.ihsan@gmail.com>
2023-10-10 04:25:56 +0000

    python-input_output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is the read_file function.
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file and prints it to stdout.
+    """
+
+    with open(filename, encoding='utf-8') as file:
+        for line in file:
+            print(line, end='')
