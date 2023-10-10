a1dd1ad28759d2c3b326ff00f17593c03bd51b71
nashi-san <hamouda.ihsan@gmail.com>
2023-10-10 13:57:25 +0000

    python-input_output

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" My class module
+"""
+
+
+class Student:
+    """
+    This class represents a student.
+    """
+
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new instance of the Student class.
+        """
+        
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of the Student instance.
+        """
+        
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
