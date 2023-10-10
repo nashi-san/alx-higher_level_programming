ed69ba07591486934eb34d74e6f236a5fc562bb0
nashi-san <hamouda.ihsan@gmail.com>
2023-10-10 13:19:36 +0000

    python-input_output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,7 +6,8 @@ This is the write_file function.
 
 def write_file(filename="", text=""):
     """
-    Writes a string to a text file and returns the number of characters written.
+    Writes a string to a text file and returns the number
+    of characters written.
     """
 
     num_chars_written = 0
