693bea05b5fea9bb402e31c26b9966004f8269a9
nashi-san <hamouda.ihsan@gmail.com>
2023-10-16 14:19:19 +0000

    almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -130,4 +130,5 @@ class Rectangle(Base):
         Returns a string representation of the Rectangle instance.
         """
 
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+        return ("[Rectangle] ({}) {}/{} - {}/{}"
+                .format(self.id, self.x, self.y, self.width, self.height))
