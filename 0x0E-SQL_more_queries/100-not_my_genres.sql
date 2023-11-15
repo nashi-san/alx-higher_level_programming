77ab52d58984c91b81c8260ac36d2308aa17157a
nashi-san <hamouda.ihsan@gmail.com>
2023-11-15 23:28:53 +0000

    SQL_more_queries

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -5,5 +5,5 @@ WHERE tv_genres.id NOT IN (
     SELECT tv_show_genres.genre_id
     FROM tv_show_genres
     JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-    WHERE tv_shows.title = 'Dexter'
+    WHERE tv_shows.title = "Dexter"
 )
