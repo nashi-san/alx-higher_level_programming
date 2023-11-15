7546a229d48c712cc6f245d9f530575cbbec006c
nashi-san <hamouda.ihsan@gmail.com>
2023-11-15 23:19:44 +0000

    SQL_more_queries

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
-SELECT tv_shows.title, IFNULL(tv_genres.name, 'NULL')
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_genres ON tv_shows.genre_id = tv_genres.id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
