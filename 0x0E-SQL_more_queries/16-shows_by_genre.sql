91662d1752700834b9c0fcfda3250cc979961fb4
nashi-san <hamouda.ihsan@gmail.com>
2023-11-15 23:17:07 +0000

    SQL_more_queries

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,4 +2,4 @@
 SELECT tv_shows.title, IFNULL(tv_genres.name, 'NULL') AS name
 FROM tv_shows
 LEFT JOIN tv_genres ON tv_shows.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, genre ASC;
+ORDER BY tv_shows.title ASC, name ASC;
