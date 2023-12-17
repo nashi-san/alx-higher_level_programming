495624522fc5139a9ded5db4f43d53eb96889739
nashi-san <hamouda.ihsan@gmail.com>
2023-12-17 22:48:23 +0000

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,8 +7,11 @@ username = sys.argv[1]
 password = sys.argv[2]
 database = sys.argv[3]
 
-db = MySQLdb.connect(host="localhost",
-                     port=3306, user=username, passwd=password, db=database)
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306, user=username,
+                         passwd=password, db=database)
 
 cur = db.cursor()
 
