98dfa17f2504d7692a80b0a64cba283c35a80c66
nashi-san <hamouda.ihsan@gmail.com>
2023-12-17 22:52:18 +0000

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
 # This script lists all states from the database hbtn_0e_0_usa
+# in ascending order
+# the script connect to a MySQL server running on localhost at port 3306
 import sys
 import MySQLdb
 
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-
 
 if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
     db = MySQLdb.connect(host="localhost",
                          port=3306, user=username,
                          passwd=password, db=database)
-
-cur = db.cursor()
-
-cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-cur.close()
-db.close()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
