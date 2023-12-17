87317c648718e2303c1f2e4c7579d7003930b218
nashi-san <hamouda.ihsan@gmail.com>
2023-12-17 23:32:21 +0000

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
                          port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE \
-                name LIKE 'N%' ORDER BY states.id ASC")
+                name LIKE 'N%' ORDER BY states.id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
