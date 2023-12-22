bca3df6620bbc133637cccacde5ffbc952946a5d
nashi-san <hamouda.ihsan@gmail.com>
2023-12-22 19:49:08 +0000

    object_relational_mapping t2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
                          port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE \
-                name LIKE 'N%' OR name LIKE 'n%' \
+                name LIKE 'N%' COLLATE utf8_bin \
                 ORDER BY id")
     rows = cur.fetchall()
     for row in rows:
