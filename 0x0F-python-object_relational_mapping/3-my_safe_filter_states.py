96231eedba0c633fcf60970b4b1094c47878e8ba
nashi-san <hamouda.ihsan@gmail.com>
2023-12-22 20:20:09 +0000

    object_relational_mapping t2

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This script displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument (safe from MySQL injections).
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
+    )
+    cur = db.cursor()
+
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id"
+    cur.execute(query, (state_name,))
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
