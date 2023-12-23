934a6d38364093692d2ce3a9541619b13bba715a
nashi-san <hamouda.ihsan@gmail.com>
2023-12-23 00:42:04 +0000

    task 101 t2

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -5,8 +5,9 @@ Lists all City objects from the database hbtn_0e_101_usa
 
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
+from relationship_city import City
 
 
 if __name__ == "__main__":
