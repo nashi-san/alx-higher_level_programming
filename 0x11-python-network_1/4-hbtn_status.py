84fb1473a37a6c8e52543bc8145beef906bad3a5
nashi-san <hamouda.ihsan@gmail.com>
2024-01-08 00:28:21 +0000

    python-network_1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Python script that takes in a URL,
+sends a request to the URL and displays the body of the response
+(decoded in utf-8).
+"""
+
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    req = requests.get(url)
+    body = req.content
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
