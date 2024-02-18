2413d6814e22c11b4388c9357021a82809014ca1
nashi-san <hamouda.ihsan@gmail.com>
2024-02-18 23:44:22 +0000

    python rel mapping

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -17,9 +17,8 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).order_by(State.id).all()
+    states = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
     for state in states:
-        print('{}: {}'.format(state.id, state.name))
+        print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print('\t{}: {}'.format(city.id, city.name))
-    session.close()
+            print("    {}: {}".format(city.id, city.name))
