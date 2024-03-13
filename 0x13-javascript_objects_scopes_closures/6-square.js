e860c2f0d06bc4da390cb4fea37e6f7e52e17442
nashi-san <hamouda.ihsan@gmail.com>
2024-03-13 02:16:14 +0000

    javascript obj

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const Square = require('./5-square');
+module.exports = class Square2 extends Square {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
