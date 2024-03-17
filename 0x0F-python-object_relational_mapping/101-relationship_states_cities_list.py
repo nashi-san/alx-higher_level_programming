861439ec8de7c31220bf48b23a16a4b88a962d1e
nashi-san <hamouda.ihsan@gmail.com>
2024-03-17 23:49:33 +0000

    o_r_mapping

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -7,6 +7,7 @@ import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from relationship_state import Base, State
+from relationship_city import City
 
 if __name__ == "__main__":
 
