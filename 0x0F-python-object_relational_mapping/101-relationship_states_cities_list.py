a831ea06fa7c5fc0fafa425a847a5a6467b1cfd4
nashi-san <hamouda.ihsan@gmail.com>
2024-03-18 00:23:03 +0000

    o_r_mapping

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -20,3 +20,4 @@ if __name__ == "__main__":
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
             print("    {}: {}".format(city.id, city.name))
+    session.close()
