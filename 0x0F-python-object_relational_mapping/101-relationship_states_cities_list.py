3b49043e63c9b716782e24f5cc0ed56253faf66e
nashi-san <hamouda.ihsan@gmail.com>
2024-03-18 00:08:54 +0000

    o_r_mapping

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -13,15 +13,15 @@ from relationship_city import City
 if __name__ == "__main__":
 
     username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
-    engine_url = "mysql+mysqldb://" + username + ":" + password + \
-        "@localhost:3306/" + db_name
+    engine_url = f"mysql+mysqldb://{username}:{password}\
+        @localhost:3306/{db_name}"
     engine = create_engine(engine_url, pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     states = session.query(State).order_by(State.id).all()
     for state in states:
-        print('{}: {}'.format(state.id, state.name))
+        print(f'{state.id}: {state.name}')
         for city in state.cities:
-            print('\t{}: {}'.format(city.id, city.name))
+            print(f'\t{city.id}: {city.name}')
     session.close()
